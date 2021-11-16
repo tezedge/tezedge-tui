f18ddec11dbad004ad9dd62e705986fd54af4d98
Adrian Nagy <adrian.nagy@viablesystems.io>
2021-11-16 09:01:23 +0100

    rework websocket handler (no unwraps)

--- src/websocket.rs
+++ src/websocket.rs
@@ -1,38 +1,42 @@
+
 use tokio_tungstenite::connect_async;
 use tokio::task::JoinHandle;
 use futures_util::stream::StreamExt;
 
 use crate::model::StateRef;
 
-// TODO: refactor this
-pub async fn spawn_ws_reader(state: StateRef) -> JoinHandle<()>{
+pub async fn spawn_ws_reader(state: StateRef) -> JoinHandle<()> {
     tokio::spawn(async move {
         let (ws_stream, _) = connect_async("ws://116.202.128.230:4927").await.expect("Failed to connect");
         let (_, read) = ws_stream.split();
         let state = state.clone();
+
         read.for_each(|raw_message| async {
-            let data: serde_json::Value = serde_json::from_str(&raw_message.unwrap().to_string()).unwrap();
-            if let Some(messages_array) = data.as_array() {
-                for message in messages_array {
-                    if let Some(message_obj) = message.clone().as_object() {
-                        if let Some(str) = &message_obj["type"].as_str() {
-                            let mut state = state.write().unwrap();
-                            match *str {
-                                "incomingTransfer" => {
-                                    state.update_incoming_transfer(serde_json::from_value(message["payload"].clone()).unwrap())
-                                },
-                                "peersMetrics" => {
-                                    state.update_peer_metrics(serde_json::from_value(message["payload"].clone()).unwrap())
-                                }
-                                "blockApplicationStatus" => {
-                                    state.update_application_status(serde_json::from_value(message["payload"].clone()).unwrap())
-                                }
-                                _ => (),
-                            }
-                        }
-                    }
-                }
-            }
-        }).await;
+            raw_message.and_then(|message| {
+                Ok(serde_json::from_str(&message.to_string()).and_then(|data: serde_json::Value| {
+                    Ok(data.as_array().and_then(|message_array| {
+                        Some(for message in message_array {
+                            message.clone().as_object().and_then(|message_obj| {
+                                message_obj["type"].as_str().and_then(|type_str| {
+                                    let mut state = state.write().unwrap();
+                                    match type_str {
+                                        "incomingTransfer" => {
+                                            Some(state.update_incoming_transfer(serde_json::from_value(message["payload"].clone()).unwrap()))
+                                        },
+                                        "peersMetrics" => {
+                                            Some(state.update_peer_metrics(serde_json::from_value(message["payload"].clone()).unwrap()))
+                                        }
+                                        "blockApplicationStatus" => {
+                                            Some(state.update_application_status(serde_json::from_value(message["payload"].clone()).unwrap()))
+                                        }
+                                        _ => None,
+                                    }
+                                })
+                            });
+                        })
+                    }))
+                }))
+            }).ok();
+        }).await
     })
-} 
+}
